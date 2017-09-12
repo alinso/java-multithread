9d963da59809ec063c570f54d75096207204aeb5
user49 <user49@USER49.key.net.tr>
2017-09-12 14:38:11 +0300

    Ex7_ReentrantLock class is formed to a decent shape.

--- multithread/src/multithread/Ex7_ReentrantLock.java
+++ multithread/src/multithread/Ex7_ReentrantLock.java
@@ -1,38 +1,46 @@
 package multithread;
-
 import java.util.concurrent.locks.*;
 
-public class Lock2 {
+
+
+//Reentrant locks are similar to synchronized blocks
+//By locking and unlocking the lock, we manage the synchronization of code block between threads.
+//If we remove the lock.lock() and lock.unlock() methods we will see that the counter cannot hit 2000.
+public class Ex7_ReentrantLock {
 
 	private static int counter = 0;
 	private static ReentrantLock lock  = new ReentrantLock();
 	
 	public static void increment() {
-		lock.lock();
 		
+		//After this line no other thread can enter the following seciton
+		lock.lock();
 		try {
 		for(int i=0;i<1000;i++) {
 		counter++;
 		}
 		}
 		finally {
+			//After this line the last lock is released so the restriction is over.
 			lock.unlock();
 		}
 	}
+	
+	
 	public static void main(String[] args) {
 		Thread t1 = new Thread(new Runnable() {
 			public void run() {
 				increment();
 			}
-			
 		});
 		
 		Thread t2= new Thread(new Runnable() {
 			public void run() {
 				increment();
 			}
-			
 		});
+	
+		
 		
 		t1.start();
 		t2.start();
@@ -41,7 +49,6 @@ public class Lock2 {
 			t1.join();
 			t2.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
