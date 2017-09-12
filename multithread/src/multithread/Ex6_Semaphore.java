44f5ab71c593bdc462529874b9b2e9a6ee606845
user49 <user49@USER49.key.net.tr>
2017-09-12 14:31:07 +0300

    Ex6_Semaphore class is formed to good shape.

--- multithread/src/multithread/Ex6_Semaphore.java
+++ multithread/src/multithread/Ex6_Semaphore.java
@@ -4,8 +4,12 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 
+
+//Semaphore decides how many threads can run the code at the same time.
 enum Downloader{
 	INSTANCE;
+	
+	//This smp oject allows only 3 threads to run this method concurrently
 	private Semaphore smp  =new Semaphore(3,true);
 	public void downloadData() {
 		try {
@@ -29,9 +33,17 @@ enum Downloader{
 	}
 }
 
-public class SemaphoreSample {
+
+
+
+
+public class Ex6_Semaphore {
 
 	public static void main(String[] args) {
+		
+		
+		//ExecutorService create and run threads.
+		//We can see in console that this code will work 3 by 3
 		ExecutorService executorService =  Executors.newCachedThreadPool();
 		for(int i=0;i<12;i++) {
 			executorService.execute(new Runnable() {
